2ed8eb85a3813bb4396f2d87c6d29e235cb74a31
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-12 14:24:13 +0300

    Начальный коммит

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите номер четверти: ");
+double n = Convert.ToDouble(Console.ReadLine());
+if (n == 1) {
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if (n == 2) {
+    Console.WriteLine("X < 0, Y > 0");
+}
+else if (n == 3) {
+    Console.WriteLine("X < 0, Y < 0");
+} 
+else if (n == 4) {
+    Console.WriteLine("X > 0, Y < 0");
+} else {
+    Console.WriteLine("Такой четверти нет");
+}
+
+ 
