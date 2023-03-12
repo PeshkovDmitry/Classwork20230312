9765d30e358e6aa039beaa42c884e28e752625a9
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-12 14:51:42 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{n} -> ");
+for (int i = 1; i <= n; i++ ) {
+    if (i == n) {
+        Console.WriteLine($"{i*i}");    
+    } else 
+    {
+        Console.Write($"{i*i},");
+    }    
+}
